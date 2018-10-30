6d9f1f497c1d1a545bfe377240abe50861a96824
PaulDL123 <pauldk95@gmail.com>
2018-10-30 10:02:50 -0500

    Header and nav

---
+++ partes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <img src="accesorios/1.jpg" alt="" height="450">
+</header>
---
+++ partes/navegador.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php">Productos</a></li>
+        <li><a href="index.php">Diseñar</a></li>
+    </ul>
+</nav>
